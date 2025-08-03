2c44dc0fb1472536cb4e79d109da91f128ec036e
Migilian <104717241+Migilian@users.noreply.github.com>
2025-08-03 20:58:42 +0300

    Mhm

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+/** @type {{minecraft: Map<string, string>, custom: Map<string, string>}} */
+export const colorMap = {
+    minecraft: new Map([
+        ["black", "#000000"],
+        ["dark_blue", "#0000AA"],
+        ["dark_green", "#00AA00"],
+        ["dark_aqua", "#00AAAA"],
+        ["dark_red", "#AA0000"],
+        ["dar_purple", "#AA00AA"],
+        ["gold", "#FFAA00"],
+        ["gray", "#AAAAAA"],
+        ["dark_gray", "#555555"],
+        ["blue", "#5555FF"],
+        ["green", "#55FF55"],
+        ["aqua", "#55FFFF"],
+        ["red", "#FF5555"],
+        ["light_purple", "#FF55FF"],
+        ["yellow", "#FFFF55"],
+        ["white", "#FFFFFF"],
+    ]),
+};
